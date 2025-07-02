6be154512d48e0eb7af1d919dee7c23777e5269c
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2025-07-01 22:42:00 -0400

    changes port

--- app/server.js
+++ app/server.js
@@ -11,7 +11,7 @@ const becomeAnAuthorRouter = require("./router/becomeAnAuthorRouter");
 const favoriteItemRouter = require("./router/favoriteItemRouter");
 const teamMemberRouter = require("./router/teamMemberRouter");
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(
