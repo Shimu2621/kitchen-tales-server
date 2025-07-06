4d74ce0cc986f5b164c7d2f8061ea72b8efc1f74
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2025-07-06 12:38:41 -0400

    changes server.js file

--- app/server.js
+++ app/server.js
@@ -43,12 +43,10 @@ app.get("/", (req, res) => {
 // });
 
 // Only run the server locally
-if (process.env.NODE_ENV !== "production") {
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-}
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 // ✅ Only this line should be exported for Vercel
 module.exports = serverless(app);
